1127c1a7c751c5755b3d29d9f1237dd42f671617
Miguel Nievas <miguel.nievas.fuertes@gmail.com>
2022-11-22 14:03:04 +0100

    W1 D2 class

---
+++ 1-Ramp_Up/Python/1-Basics/Teoria/pizzas_online.py
@@ -0,0 +1,9 @@
+print("Buenas tardes, bienvenido al servicio de pedido online, ¿Cuántas pizzas desea?")
+
+pizz = input()
+# print(pizz)
+
+direcc = input(f"Estupendo,\n se están preparando {pizz} pizzas. Digame su dirección")
+# print(direcc)
+
+print(f"Le mandaremos las {pizz} pizzas a la dirección {direcc}. Muchas gracias por su pedido.")
