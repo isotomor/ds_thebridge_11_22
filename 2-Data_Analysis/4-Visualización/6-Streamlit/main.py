a31a4970a45d112920c2b96584ebee0ea48606d6
Miguel Nievas <miguel.nievas.fuertes@gmail.com>
2023-01-13 13:41:10 +0100

    W6 D6

--- 2-Data_Analysis/4-Visualización/6-Streamlit/main.py
+++ 2-Data_Analysis/4-Visualización/6-Streamlit/main.py
@@ -1,7 +1,23 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image # !pip install Pillow
 
 st.set_page_config(page_title="Cargadores", page_icon=":electric_plug:", layout="wide")
 
-path = "red_recarga_acceso_publico_2021.csv"
+path = "data/red_recarga_acceso_publico_2021.csv"
 df = pd.read_csv(path, sep=";")
+df.rename(columns={"latidtud":"lat", "longitud":"lon"}, inplace=True)
+
+img = Image.open('data/puntos-recarga-madrid.jpg')
+
+st.title("Cargadores Madrid")
+
+menu = st.sidebar.selectbox("Selecciona la página", ['Home','Datos','Filtros'])
+
+if menu == "Home":
+    st.image(img, use_column_width='auto')
+    with st.expander('Quieres saber más?'):
+        st.write("Es una solución factible para reducir emisiones en la ciudad")
+elif menu == "Datos":
+    st.write(df.head(5))
+    st.map(df)
