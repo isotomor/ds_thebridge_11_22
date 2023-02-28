2347d1d7487592fec913bff04a54522ca9f3c057
Miguel Nievas <miguel.nievas.fuertes@gmail.com>
2023-02-28 10:51:45 +0100

    W13 D2 class

--- 4-Data_Engineering/1-APIs/BBDD/ejercicio/app_bd.py
+++ 4-Data_Engineering/1-APIs/BBDD/ejercicio/app_bd.py
@@ -22,9 +22,63 @@ def get_all():
     return jsonify(result)
 
 # 1.Ruta para obtener el conteo de libros por autor ordenados de forma descendente
-
+@app.route('/api/v1/resources/booksbyauthor/', methods=['GET'])
+def author_books():
+    connection = sqlite3.connect('books.db')
+    cursor = connection.cursor()
+    select_books = '''
+    SELECT 
+        author,
+        COUNT(*) 
+    FROM books 
+    GROUP BY 1 
+    ORDER BY 2 DESC'''
+    result = cursor.execute(select_books).fetchall()
+    connection.close()
+    return jsonify(result)
 
 # 2.Ruta para obtener los libros de un autor como argumento en la llamada
+@app.route('/api/v1/resources/books/author', methods=['GET'])
+def book_author():
+    if 'author' in request.args:
+        author = "%"+request.args['author']+"%"
+        connection = sqlite3.connect('books.db')
+        cursor = connection.cursor()
+        select_books = '''
+        SELECT 
+            * 
+        FROM books 
+        WHERE author LIKE ?'''
+        result = cursor.execute(select_books, (author,)).fetchall()
+        connection.close()
+        return jsonify(result)
+    else:
+        return "Author argument missing"
 
 
 # 3.Ruta para obtener los libros filtrados por título, publicación y autor
+@app.route('/api/v1/resources/books/filters', methods=['GET'])
+def filters():    
+    query = "SELECT * FROM books WHERE"
+    filters = []
+    if 'title' not in request.args and 'published' not in request.args and 'author' not in request.args:
+        return "Filters missing"
+    else:
+        if 'title' in request.args:
+            title = '%' + request.args['title'] + '%'
+            query += " title LIKE ? AND"
+            filters.append(title)
+        if 'published' in request.args:
+            published = request.args['published']
+            query += " published = ? AND"
+            filters.append(published)
+        if 'author' in request.args:
+            author = '%' + request.args['author'] + '%'
+            query += " author LIKE ? AND"
+            filters.append(author)
+        connection = sqlite3.connect('books.db')
+        cursor = connection.cursor()
+        result = cursor.execute(query[:-4], filters).fetchall()
+        return jsonify(result)
+
+app.run()
---
+++ 4-Data_Engineering/1-APIs/Model/ejercicio/app_model_db.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import os
+import pickle
+from sklearn.model_selection import cross_val_score
+import pandas as pd
+import sqlite3
+
+
+os.chdir(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "Bienvenido a mi API del modelo advertising"
+
+
+app.run()
